10d36f1ab2c4e03eba5f488d275f0d1043577268
Unknown <ppsammartino@gmail.com>
2017-12-20 13:44:13 -0300

    Fixed: Now client/project filter querysets are first filtered by date.

--- log/filters/filterbyclient.py
+++ log/filters/filterbyclient.py
@@ -1,13 +1,20 @@
 from django.contrib.admin import SimpleListFilter
 from .filterbyproject import FilterEntriesByProject
-from log.models import Client
+from log.models import Client, Entry, Project
 
 
 class FilterEntriesByClient(FilterEntriesByProject):
 
     def get_lookup_choices(self, changelist):
+        qs = Entry.objects.all()
+        if changelist.params:
+            args = {}
+            for key in changelist.params:
+                if 'date' in key:
+                    args[key] = changelist.params[key]
+            qs = qs.filter(**args)
         projects_available = list(
-            set([project for project in changelist.queryset.values_list('project_id', flat=True)]))
+            set([project for project in qs.values_list('project_id', flat=True)]))
         self.lookup_choices = Client.objects.filter(project__in=projects_available).values_list('id', 'name')
 
     title = u'Client'
